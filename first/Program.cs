6406c6c9cf8a9c1d247ec12c7f49694ba8a4209a
krushnaDevatwal <krushnadevatwal@gmail.com>
2023-12-10 09:00:20 +0530

    Add project files.

---
+++ first/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Shri Ganesh");
+            Console.ReadLine();
+        }
+    }
+}
